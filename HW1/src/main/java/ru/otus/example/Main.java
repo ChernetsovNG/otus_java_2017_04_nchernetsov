bf488595df2750527f29243b261b36dbdc5a4aad
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-01 12:53:53 +0300

    Add example reading from .csv file

--- HW1/src/main/java/ru/otus/example/Main.java
+++ HW1/src/main/java/ru/otus/example/Main.java
@@ -1,7 +1,27 @@
 package ru.otus.example;
 
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Otus!");
+    public static void main(String[] args) throws IOException {
+
+        String pathToFile = "HW1/src/main/resources/example.csv";
+        char cvsSplitBy = ';';
+
+        CSVReader reader = new CSVReader(new FileReader(pathToFile), cvsSplitBy);
+
+        String[] strings;
+
+        while ((strings = reader.readNext()) != null) {
+            for (String s : strings) {
+                if (s.equals(""))  //пустые строки пропускаем
+                    continue;
+                System.out.println(s.trim());
+            }
+        }
+
     }
 }

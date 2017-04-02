72194030c8aaa553e90badef028192dac3fdae43
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-02 13:14:31 +0300

    Поправил путь к файлу ресурсов

--- HW1/src/main/java/ru/otus/example/Main.java
+++ HW1/src/main/java/ru/otus/example/Main.java
@@ -3,15 +3,18 @@ package ru.otus.example;
 import ru.otus.util.CSVutil;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String pathToFile = "HW1/src/main/resources/example.csv";
+        String pathToFile = "/example.csv";
         char csvSplitBy = ';';
 
-        List<String> strings = CSVutil.readFromCSV(pathToFile, csvSplitBy);
+        CSVutil csVutil = new CSVutil();
+
+        List<String> strings = csVutil.readFromCSV(pathToFile, csvSplitBy);
 
         for (String s : strings) {
             System.out.println(s);
--- HW1/src/main/java/ru/otus/util/CSVutil.java
+++ HW1/src/main/java/ru/otus/util/CSVutil.java
@@ -2,16 +2,17 @@ package ru.otus.util;
 
 import au.com.bytecode.opencsv.CSVReader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVutil {
 
-    public static List<String> readFromCSV(String pathToFile, char csvSplitBy) throws IOException {
-        CSVReader reader = new CSVReader(new FileReader(pathToFile), csvSplitBy);
+    public List<String> readFromCSV(String pathToFile, char csvSplitBy) throws IOException {
+
+        InputStream resourceFile = getClass().getResourceAsStream(pathToFile);
+        CSVReader reader = new CSVReader(new InputStreamReader(resourceFile), csvSplitBy);
+
         List<String> result = new ArrayList<>();
         String[] strings;
         while ((strings = reader.readNext()) != null) {

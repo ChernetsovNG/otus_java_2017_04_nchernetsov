a8f3b2229b476529553d408c36ab74dbd5970e44
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-08 17:12:06 +0300

    Поправил пакет main

--- HW2/src/main/java/ru/otus/measure/InstrumentationAgent.java
+++ HW2/src/main/java/ru/otus/measure/InstrumentationAgent.java
@@ -7,18 +7,16 @@ public class InstrumentationAgent {
     private static volatile Instrumentation instrumentation;
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("premain...");
         instrumentation = inst;
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
-        System.out.println("agentmain...");
         instrumentation = inst;
     }
 
     public static void printSize(final Object object) {
-        System.out.println("Object of type: " + object.getClass() +
-                " has size of: " + getObjectSize(object) + " bytes");
+        System.out.println("Object: " + object + " of type: " + object.getClass() +
+                " has size of: " + getObjectSize(object) + " bytes\n");
     }
 
     public static long getObjectSize(final Object object) {

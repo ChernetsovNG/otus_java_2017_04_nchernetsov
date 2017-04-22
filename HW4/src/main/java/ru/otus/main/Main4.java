f94964d5d444365fcbd32b9b87f0c3316fb3e93f
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-22 16:05:03 +0300

    Добавил некий прототип с информацией о работе сборщика мусора

--- HW4/src/main/java/ru/otus/main/Main4.java
+++ HW4/src/main/java/ru/otus/main/Main4.java
@@ -1,7 +1,41 @@
 package ru.otus.main;
 
+import java.lang.management.ManagementFactory;
+
+import static ru.otus.utils.MemoryUtil.*;
+
 public class Main4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+
+        int size = 5 * 1000 * 1000;
+
+        Object[] array = new Object[size];
+        System.out.println("Array of size: " + array.length + " created");
+
+        int n = 0;
+        int currentSize = size;
+        System.out.println("Starting the loop");
+
+        //startGCMonitor();
+        installGCMonitoring();
+
+        while (n < Integer.MAX_VALUE) {
+            int i = n % currentSize;
+            array[i] = new String(new char[0]);
+            n++;
+            if (n % currentSize == 0) {
+                Thread.sleep(1000);
+                System.out.println("Created " + n + " objects");
+                System.out.println("Creating new array of size: " + size);
+                printUsage(true);
+                currentSize = size;
+                array = new Object[currentSize];
+            }
+        }
+
+        //stopGCMonitor();
 
     }
+
 }

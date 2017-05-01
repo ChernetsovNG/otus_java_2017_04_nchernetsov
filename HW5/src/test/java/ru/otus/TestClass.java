2c5a6d61534b588dc5eff28ead5b9b7e9e5beb7a
ChernetsovNG <n.chernetsov86@gmail.com>
2017-05-01 12:02:24 +0300

    Создал папку HW5 и добавил в неё ReflectionHelper и его тесты, а также примеры аннотаций из L5.1

---
+++ HW5/src/test/java/ru/otus/TestClass.java
@@ -0,0 +1,32 @@
+package ru.otus;
+
+@SuppressWarnings("unused")
+public class TestClass {
+    private int a = 0;
+    private String s = "";
+
+    public TestClass() {
+    }
+
+    public TestClass(Integer a) {
+        this.a = a;
+    }
+
+    public TestClass(Integer a, String s) {
+        this.a = a;
+        this.s = s;
+    }
+
+    int getA() {
+        return a;
+    }
+
+    String getS() {
+        return s;
+    }
+
+    private void setDefault(){
+        a = 0;
+        s = "";
+    }
+}

b64676a84eb902cfe9a0537c218875e687e69ff1
chernetsov <chernetsov@glosav.ru>
2017-06-29 12:16:05 +0300

    HW12: первый вариант с просмотром статистики кеша заработал

--- HW12/src/main/java/ru/otus/servlet/CacheInfoServlet.java
+++ HW12/src/main/java/ru/otus/servlet/CacheInfoServlet.java
@@ -9,31 +9,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CacheInfoServlet extends HttpServlet {
+    private static final String ACCESS_DENIED_PAGE_TEMPLATE = "access_denied.html";
     private static final String CACHE_INFO_PAGE_TEMPLATE = "cache_info.html";
 
-    private static final String DEFAULT_LOGIN = "unknown";
-    private static final String DEFAULT_PASSWORD = "unknown password";
+    private static final String HARDCODED_LOGIN = "admin";
+    private static final String HARDCODED_PASSWORD = "12345";
 
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException, IOException {
+    private int[] cacheStats;
 
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
-
-        response.getWriter().println(TemplateProcessor.instance().getPage(CACHE_INFO_PAGE_TEMPLATE, pageVariables));
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+    public CacheInfoServlet(int[] cacheStats) {
+        this.cacheStats = cacheStats;
     }
 
-    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
-        Map<String, Object> pageVariables = new HashMap<>();
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
 
         String login = request.getParameter(AuthServlet.LOGIN_PARAMETER_NAME);
         String password = request.getParameter(AuthServlet.PASSWORD_PARAMETER_NAME);
 
-        pageVariables.put("login", login != null ? login : DEFAULT_LOGIN);
-        pageVariables.put("password", password != null ? password : DEFAULT_PASSWORD);
+        if (!login.equals(HARDCODED_LOGIN) || !password.equals(HARDCODED_PASSWORD)) {
+            response.getWriter().println(TemplateProcessor.instance().getPage(ACCESS_DENIED_PAGE_TEMPLATE, null));
+
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_NON_AUTHORITATIVE_INFORMATION);
+        } else {
+            Map<String, Object> pageVariables = createPageVariablesMap(request);
+
+            response.getWriter().println(TemplateProcessor.instance().getPage(CACHE_INFO_PAGE_TEMPLATE, pageVariables));
+
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+
+    private Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+
+        pageVariables.put("hit_count", cacheStats[0]);
+        pageVariables.put("miss_count", cacheStats[1]);
+        pageVariables.put("element_count", cacheStats[2]);
 
         return pageVariables;
     }
+
 }
--- HW12/src/main/java/ru/otus/servlet/Main.java
+++ HW12/src/main/java/ru/otus/servlet/Main.java
@@ -5,6 +5,13 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import ru.otus.base.dataSets.AddressDataSet;
+import ru.otus.base.dataSets.PhoneDataSet;
+import ru.otus.base.dataSets.UserDataSet;
+import ru.otus.dbService.DBService;
+import ru.otus.dbService.DBServiceHibernateImpl;
+
+import java.util.Collections;
 
 public class Main {
     private final static int PORT = 8090;
@@ -16,8 +23,21 @@ public class Main {
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
+        // Create database (and cache)
+        DBService dbService = new DBServiceHibernateImpl();
+
+        for (int i = 1; i <= 6; i++) {
+            UserDataSet userDataSet = new UserDataSet("User " + i,
+                new AddressDataSet("Street " + i, i),
+                Collections.singletonList(new PhoneDataSet(i, String.valueOf(i))));
+
+            dbService.save(userDataSet);
+        }
+        dbService.read(1);
+        dbService.read(2);
+
         context.addServlet(new ServletHolder(new AuthServlet("unknown", "unknown")), "/auth");
-        context.addServlet(CacheInfoServlet.class, "/cache_info");
+        context.addServlet(new ServletHolder(new CacheInfoServlet(dbService.getCacheStats())), "/cache_info");
 
         Server server = new Server(PORT);
 
@@ -25,5 +45,7 @@ public class Main {
 
         server.start();
         server.join();
+
+        dbService.shutdown();
     }
 }
